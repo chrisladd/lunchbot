b77a9a454f58b05234c43362eee00fe3a40edce9
Chris Ladd <christopher.ladd@nytimes.com>
2016-03-23 11:28:50 -0400

    adds lines

--- lunchbot.rb
+++ lunchbot.rb
@@ -135,7 +135,7 @@ def responseForString(arg_string)
   if stationIds.count > 0
     stationIds.each do |id|
       begin
-        stationSpecificText += "*#{menu[:stations][id][:display]}*:\n"
+        stationSpecificText += "#{menu[:stations][id][:display]}:\n―――\n"
         stationSpecificText += stringForStationForDay(menu, id, targetDate, dietaryFilter)
         stationSpecificText += "\n"
       rescue
